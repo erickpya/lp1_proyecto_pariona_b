1c46dbc51c7a25b2aac8c6234efaff21196d55a3
Epy <epy@cibertec.edu.pe>
2026-03-29 13:29:04 -0500

    Termino de crud de tabla countries

--- src/main/java/com/cibertec/utils/ConstantesQuery.java
+++ src/main/java/com/cibertec/utils/ConstantesQuery.java
@@ -1,30 +1,28 @@
 package com.cibertec.utils;
 
 public class ConstantesQuery {
-	
-	/*INICIO QUERY PARA LA TABLA COUNTRIES*/
+
+	/* INICIO QUERY PARA LA TABLA COUNTRIES */
 	// insertar
 	// insert into countries(country_name, region_id) values (?,?)
-	public static final String QUERY_COUNTRIES_INSERTAR 
-			= "insert into countries(country_name, region_id) values (?,?)";
+	public static final String QUERY_COUNTRIES_INSERTAR = "insert into countries(country_id, country_name, region_id) values (?,?,?)";
 	// consultar todo
 	// select country_id, country_name, region_id from countries
-	
+	public static final String QUERY_COUNTRIES_CONSULTAR_TODO = "select country_id, country_name, region_id from countries";
 	// eliminar
 	// delete from countries where country_id= ?
-	
+	public static final String QUERY_COUNTRIES_ELIMINAR = "delete from countries where country_id= ?";
 	// consultar por id
 	// select country_id, country_name, region_id from countries
 	// where country_id=?
-	
+	public static final String QUERY_COUNTRIES_CONSULTAR_X_ID = "select country_id, country_name, region_id from countries where country_id=?";
 	// actualizar
 	// update countries set country_name=?, region_id=?
 	// where country_id=?
-	
-	
-	/*FIN QUERY PARA LA TABLA COUNTRIES*/
+	public static final String QUERY_COUNTRIES_ACTUALIZAR = "update countries set country_name=?, region_id=? where country_id=?";
 
-	/* PARA OTRAS TABLAS ......*/
-	
-	
+	/* FIN QUERY PARA LA TABLA COUNTRIES */
+
+	/* PARA OTRAS TABLAS ...... */
+
 }
